a14bd21d110964a3d561fa9ab97388c65324e9a8
Subhajit Kundu <kundusubhajit73@gmail.com>
2025-07-04 18:44:55 +0530

    setup done and assets uploaded

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SignIn = () => {
   return (
-    <View>
+    <SafeAreaView className='bg-white h-full p-4'>
       <Text>SignIn</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
